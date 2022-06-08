41c48351ca1fa63d2314dde1b92b2d781f602554
AaronTran206 <aarontran.wa@gmail.com>
2022-06-08 02:54:44 -0600

    styles fix

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -12,57 +12,54 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <div>
-          <Typography
-            component={Link}
-            to="/"
-            className={classes.heading}
-            variant="h3"
-            align="center"
-          >
-            Memories
-          </Typography>
-          <img
-            className={classes.image}
-            src={memories}
-            alt="memories"
-            height="50"
-          />
-        </div>
-        <Toolbar className={classes.toolbar}>
-          {user ? (
-            <div className={classes.profile}>
-              <Avatar
-                className={classes.purple}
-                alt={user.result.name}
-                src={user.result.imageUrl}
-              >
-                {user.result.name.charAt(0)}
-              </Avatar>
-              <Typography className={classes.userName} variant="h6">
-                {user.result.name}
-              </Typography>
-              <Button
-                variant="container"
-                className={classes.logout}
-                color="secondary"
-                onClick={() => {}}
-              >
-                Logout
-              </Button>
-            </div>
-          ) : (
-            <Button
-              component={Link}
-              to="/auth"
-              variant="contained"
-              color="primary"
-            >
-              Sign In
-            </Button>
-          )}
-        </Toolbar>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+        >
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="container"
+              className={classes.logout}
+              color="secondary"
+              onClick={() => {}}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   )
 }
