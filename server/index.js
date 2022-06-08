817413bbbdb75d98d045f3ca8fd495629528b821
AaronTran206 <aarontran.wa@gmail.com>
2022-06-07 19:23:59 -0600

    added environmental file

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+import dotenv from "dotenv"
 
 //routes for when user visit the url
 import postRoutes from "./routes/posts.js"
 
 const app = express()
+dotenv.config()
 
 //setup
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
@@ -18,13 +20,13 @@ app.use(cors())
 app.use("/posts", postRoutes)
 
 //mongoDB Atlas
-const CONNECTION_URL =
-  "mongodb+srv://aarontran206:eMtEnGsDSHSnX6YZ@cluster0.yzrot.mongodb.net/?retryWrites=true&w=majority"
+// const CONNECTION_URL =
+//   "mongodb+srv://aarontran206:eMtEnGsDSHSnX6YZ@cluster0.yzrot.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
 //mongoose setup
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
