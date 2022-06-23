92742982d8288b564440117e4423548364f8b3c1
AaronTran206 <aarontran.wa@gmail.com>
2022-06-22 18:08:09 -0600

    redirect file added and new heroku site added to frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
 //easier to make API calls with axios
 import axios from "axios"
 
-const API = axios.create({ baseURL: "http://localhost:5000" })
+const API = axios.create({
+  baseURL: "https://atran-mern-memories-project.herokuapp.com/",
+})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
